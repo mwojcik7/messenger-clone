1aa564f7f905899034122ba3db954efc9a9f157b
Marcin Wójcik <marcin7wojcik@gmail.com>
2023-05-15 21:34:25 +0200

    feat: add user list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,12 +1,19 @@
-import Sidebar from "../components/Sidebar/Sidebar";
+import getUsers from '../actions/getUsers';
+import Sidebar from '../components/Sidebar/Sidebar';
+import UserList from './components/UserList';
 
-const UserLayout = async ({children}: {children: React.ReactNode}) => {
+const UserLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
-  )
-}
+  );
+};
 
 export default UserLayout;
